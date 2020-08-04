ec48a6ca3bcf2f3c2cbd1495d287ae779d60cd61
unknown <worldrulesdev@gmail.com>
2020-08-04 09:17:41 -0300

    botao de imprimir , rotas e materias separadas

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MateriaPrincipal from './components/materias/materiaprincipal';
+import Materia2 from './components/materias/materia2';
+import Materia3 from './components/materias/materia3';
+import Materia4 from './components/materias/materia4';
 
 const Pagina404 = () => <div>Página 404</div>;
 
@@ -11,7 +15,20 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={App} exact></Route>
-      <Route path="/materia" component={true} exact></Route>
+      <Route path="/materia" component={MateriaPrincipal} exact></Route>
+      <Route path="/materia/materia2" component={Materia2} exact></Route>
+      <Route path="/materia/materia3" component={Materia3} exact></Route>
+      <Route path="/materia/materia4" component={Materia4} exact></Route>
+      <Route
+        path="/materia/materia5"
+        component={MateriaPrincipal}
+        exact
+      ></Route>
+      <Route
+        path="/materia/materia6"
+        component={MateriaPrincipal}
+        exact
+      ></Route>
       <Route component={Pagina404}></Route>
     </Switch>
   </BrowserRouter>,
